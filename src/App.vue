1e6e92de8523669e0fde107e07c8085f1d1968d2
writeland <2664040792@qq.com>
2024-09-17 17:02:28 +0800

    feat: axios的相关配置

--- src/App.vue
+++ src/App.vue
@@ -15,15 +15,33 @@
       >登录</van-button
     >
     <van-button @click="store.delUser" type="primary">退出</van-button>
+
+    <van-button @click="getUser">获取用户信息</van-button>
+    <van-button @click="login">接口登录</van-button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+// import { ref, reactive } from 'vue'
 import { Button as VanButton } from 'vant'
 import { useUserStore } from './stores'
+import { request } from './utils/request'
+import type { User } from './types/user'
 
 const store = useUserStore()
+
+const getUser = async () => {
+  request('/patient/myUser', 'get', { test: 1 })
+  // console.log(res)
+}
+
+const login = async () => {
+  let res = await request<User>('/login/password', 'POST', {
+    mobile: '13211112222',
+    password: 'abc12345'
+  })
+  console.log(res)
+}
 </script>
 
 <style scoped lang="scss"></style>
---
+++ src/utils/request.ts
@@ -0,0 +1,73 @@
+import { useUserStore } from '@/stores'
+import router from '@/router'
+import axios, { type Method } from 'axios'
+import { showToast } from 'vant'
+
+// 1. 新axios实例，基础配置
+const instance = axios.create({
+  baseURL: 'https://consult-api.itheima.net/',
+  timeout: 10000
+})
+
+// 2. 请求拦截器，携带token
+instance.interceptors.request.use(
+  (config) => {
+    const store = useUserStore()
+    if (store.user?.token && config.headers) {
+      config.headers['Authorization'] = `Bearer ${store.user?.token}`
+    }
+    return config
+  },
+  (err) => Promise.reject(err)
+)
+
+// 3. 响应拦截器，剥离无效数据，401拦截
+instance.interceptors.response.use(
+  (res) => {
+    // 后台约定，响应成功，但是code不是10000，是业务逻辑失败
+    if (res.data?.code !== 10000) {
+      showToast(res.data?.message || '业务失败')
+      return Promise.reject(res.data)
+    }
+    // 业务逻辑成功，返回响应数据，作为axios成功的结果
+    return res.data
+  },
+  (err) => {
+    if (err.response.status === 401) {
+      // 删除用户信息
+      const store = useUserStore()
+      store.delUser()
+      // 跳转登录，带上接口失效所在页面的地址，登录完成后回跳使用
+      router.push({
+        path: '/login',
+        query: { returnUrl: router.currentRoute.value.fullPath }
+      })
+    }
+    return Promise.reject(err)
+  }
+)
+
+export default instance
+
+// 这个需要替换axsio.request默认的响应成功后的结果类型
+// 之前是：传 { name: string } 然后res是   res = { data: { name: string } }
+// 但现在：在响应拦截器中返回了 res.data  也就是将来响应成功后的结果，和上面的类型一致吗？
+// 所以要：request<数据类型，数据类型>() 这样才指定了 res.data 的类型
+// 但是呢：后台返回的数据结构相同，所以可以抽取相同的类型
+type Data<T> = {
+  code: number
+  message: string
+  data: T
+}
+
+export const request = <T>(
+  url: string,
+  method: Method = 'GET',
+  submitData?: object
+) => {
+  return instance.request<any, Data<T>>({
+    url,
+    method,
+    [method.toUpperCase() === 'GET' ? 'params' : 'data']: submitData
+  })
+}

cdef872e2849af4d6a0cb5f843bae4a61618760c
writeland <2664040792@qq.com>
2024-09-17 10:42:02 +0800

    feat: 移动端适配

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>APP</div>
+  <div>
+    <vant-button @click="clickBtn" type="danger">按钮</vant-button>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
+import { Button as VantButton } from 'vant'
+
+const clickBtn = () => {
+  alert('Click')
+}
 // a = 21
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './style/main.scss'
+import 'vant/lib/index.css'
 
 const app = createApp(App)
 

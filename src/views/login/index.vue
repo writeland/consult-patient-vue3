dc82f20de3ffc2f6cc4ae73f8ceb7ac2a2e5e475
writeland <2664040792@qq.com>
2024-09-18 10:12:04 +0800

    feat: 配置组件自动注册

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div>这是登录页面</div>
+  <van-button>按钮</van-button>
+  <van-checkbox>付现款</van-checkbox>
 </template>
 
 <script setup lang="ts">
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    // 自动导入的插件，解析器可以是 vant element and-vue
+    Components({
+      dts: false,
+      // 原因：Toast Confirm 这类组件的样式还是需要单独引入，样式全局引入了，关闭自动引入
+      resolvers: [VantResolver({ importStyle: false })]
+    }),
+    vueDevTools()
+  ],
   // base: '/hz', 设置基础路径import.meta.env.url
   resolve: {
     alias: {

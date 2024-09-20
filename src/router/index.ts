669d961c5bc31e4652a3dfc9d95a3a2527031b60
writeland <2664040792@qq.com>
2024-09-20 16:47:17 +0800

    feat: 布局容器的进度条实现

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { useUserStore } from '@/stores'
 import { createRouter, createWebHistory } from 'vue-router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -41,6 +43,8 @@ const router = createRouter({
 
 //全局前置导航守卫
 router.beforeEach((to) => {
+  // 开启进度条
+  NProgress.start()
   // 获取token
   const store = useUserStore()
   // 白名单页面
@@ -51,7 +55,14 @@ router.beforeEach((to) => {
 
 // 全局后置守卫
 router.afterEach((to) => {
+  // 设置网页标题
   document.title = `${to.meta.title || ''}-优医问诊`
+  // 关闭进度条
+  NProgress.done()
+})
+
+NProgress.configure({
+  showSpinner: false
 })
 
 // console.log(import.meta)

da592729b82ba5b8348365f5d556177014912d8c
writeland <2664040792@qq.com>
2024-09-21 09:40:31 +0800

    feat: 家庭档案-展示患者列表

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,6 +12,11 @@ const router = createRouter({
       meta: { title: '登录' }
     },
     {
+      path: '/user/patient',
+      component: () => import('@/views/User/PatientPage.vue'),
+      meta: { title: '家庭档案' }
+    },
+    {
       path: '/',
       component: () => import('@/views/Layout/index.vue'),
       children: [
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,4 +1,4 @@
-import type { CodeType, User, UserInfo } from '@/types/user'
+import type { CodeType, PatientList, User, UserInfo } from '@/types/user'
 import { request } from '@/utils/request'
 
 // 密码登录
@@ -15,4 +15,7 @@ export const sendMobileCode = (mobile: string, type: CodeType) =>
   request('code', 'GET', { mobile, type })
 
 // 获取个人用户信息
-export const getUserInfo = () => request<UserInfo>('/patient/myUser', 'GET')
+export const getUserInfo = () => request<UserInfo>('/patient/myUser')
+
+// 获取患者列表信息
+export const getPatientList = () => request<PatientList>('/patient/mylist')

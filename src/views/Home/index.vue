ed898353606ebba442d2d4630019663934f50002
writeland <2664040792@qq.com>
2024-09-22 18:13:23 +0800

    feat:极速问诊-选择问诊类型

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,9 +2,13 @@
 import { ref } from 'vue'
 import knowledgeList from './components/KnowledgeList.vue'
 import FollowDoctor from './components/FollowDoctor.vue'
+import { useConsultStore } from '@/stores'
+import { ConsultType } from '@/enums'
 
 // active 的值是 tab 的索引
 const active = ref(1)
+
+const store = useConsultStore()
 </script>
 
 <template>
@@ -29,7 +33,11 @@ const active = ref(1)
           </router-link>
         </van-col>
         <van-col span="8">
-          <router-link to="/consult/fast" class="nav">
+          <router-link
+            to="/consult/fast"
+            class="nav"
+            @click="store.setType(ConsultType.Fast)"
+          >
             <cp-icon name="home-graphic"></cp-icon>
             <p class="title">极速问诊</p>
             <p class="desc">20s医生极速回复</p>

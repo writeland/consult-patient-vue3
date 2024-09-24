4bad639fdbf543816ece200f3410e2aaa7fdc6f0
writeland <2664040792@qq.com>
2024-09-24 22:07:01 +0800

    feat:问诊室-组件拆分

--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>room</div>
+  <div class="room-page">
+    <cp-nav-bar title="问诊室" />
+    <!-- 状态栏 -->
+    <room-status />
+    <!-- 问诊情况 -->
+    <room-message />
+    <!-- 操作栏 -->
+    <room-action />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import RoomStatus from './components/RoomStatus.vue'
+import RoomAction from './components/RoomAction.vue'
+import RoomMessage from './components/RoomMessage.vue'
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.room-page {
+  padding-top: 90px;
+  padding-bottom: 60px;
+  min-height: 100vh;
+  box-sizing: border-box;
+  background-color: var(--cp-bg);
+  .van-pull-refresh {
+    width: 100%;
+    min-height: calc(100vh - 150px);
+  }
+}
+</style>

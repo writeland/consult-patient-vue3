184bc84f5fb8d1efdf862193453fb9180fcaea30
writeland <2664040792@qq.com>
2024-09-26 15:56:02 +0800

    feat:问诊室-聊天记录

--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -4,7 +4,9 @@
     <!-- 状态栏 -->
     <room-status :status="consult?.status" :countdown="consult?.countdown" />
     <!-- 问诊情况 -->
-    <room-message v-for="item in list" :key="item.id" :item="item" />
+    <van-pull-refresh v-model="loading" @refresh="onRefresh">
+      <room-message v-for="item in list" :key="item.id" :item="item" />
+    </van-pull-refresh>
     <!-- 操作栏 -->
     <room-action
       @send-text="onSendText"
@@ -27,6 +29,8 @@ import type { Message, TimeMessages } from '@/types/room'
 import { MsgType, OrderType } from '@/enums'
 import type { ConsultOrderItem, Image } from '@/types/consult'
 import { getConsultOrderDetail } from '@/services/consult'
+import dayjs from 'dayjs'
+import { showToast } from 'vant'
 
 const consult = ref<ConsultOrderItem>()
 const loadConsult = async () => {
@@ -36,6 +40,15 @@ const loadConsult = async () => {
 const store = useUserStore()
 const route = useRoute()
 const list = ref<Message[]>([])
+const initialMsg = ref(true)
+const loading = ref(false)
+const time = ref(dayjs().format('YYYY-MM-DD HH:mm:ss'))
+
+// 下拉刷新
+const onRefresh = () => {
+  socket.emit('getChatMsgList', 20, time.value, route.query.orderId)
+}
+
 let socket: Socket
 onMounted(() => {
   loadConsult()
@@ -64,7 +77,9 @@ onMounted(() => {
   socket.on('chatMsgList', ({ data }: { data: TimeMessages[] }) => {
     // data 数据 ===> [{createTime}, ...items]
     const arr: Message[] = []
-    data.forEach((item) => {
+    data.forEach((item, i) => {
+      // 记录每一段消息中最早的消息时间，获取聊天记录需要使用
+      if (i == 0) time.value = item.createTime
       arr.push({
         msgType: MsgType.Notify,
         msg: {
@@ -76,6 +91,16 @@ onMounted(() => {
       arr.push(...item.items)
     })
     list.value.unshift(...arr)
+
+    loading.value = false
+    if (!arr.length) return showToast('没有更多聊天记录了')
+    if (initialMsg.value) {
+      // 第一次需要滚动到最新的消息
+      nextTick(() => {
+        window.scrollTo(0, document.body.scrollHeight)
+        initialMsg.value = false
+      })
+    }
   })
   // console.log(list.value)
   // 监听订单状态的变化
